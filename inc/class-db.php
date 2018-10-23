55008d3652b7f3cbb43a5c9f20244c689faea88b
Nathaniel Schweinberg <natanielks@gmail.com>
2018-10-22 18:32:23 -0700

    overload wpdb::get_caller() to use class method wp_debug_backtrace_summary when the function is not available yet

--- inc/class-db.php
+++ inc/class-db.php
@@ -13,4 +13,62 @@ class DB extends wpdb {
 		}
 		return $result;
 	}
+
+	public function get_caller() {
+		if ( function_exists( 'wp_debug_backtrace_summary' ) ) {
+			return wp_debug_backtrace_summary( __CLASS__ );
+		}
+		return $this->wp_debug_backtrace_summary( __CLASS__ );
+	}
+
+	/**
+	* Return a comma-separated string of functions that have been called to get
+	* to the current point in code.
+	*
+	* @since 3.4.0
+	*
+	* @see https://core.trac.wordpress.org/ticket/19589
+	*
+	* @param string $ignore_class Optional. A class to ignore all function calls within - useful
+	*                             when you want to just give info about the callee. Default null.
+	* @param int    $skip_frames  Optional. A number of stack frames to skip - useful for unwinding
+	*                             back to the source of the issue. Default 0.
+	* @param bool   $pretty       Optional. Whether or not you want a comma separated string or raw
+	*                             array returned. Default true.
+	* @return string|array Either a string containing a reversed comma separated trace or an array
+	*                      of individual calls.
+	*/
+	function wp_debug_backtrace_summary( $ignore_class = null, $skip_frames = 0, $pretty = true ) {
+		if ( version_compare( PHP_VERSION, '5.2.5', '>=' ) )
+			$trace = debug_backtrace( false );
+		else
+			$trace = debug_backtrace();
+
+		$caller = array();
+		$check_class = ! is_null( $ignore_class );
+		$skip_frames++; // skip this function
+
+		foreach ( $trace as $call ) {
+			if ( $skip_frames > 0 ) {
+				$skip_frames--;
+			} elseif ( isset( $call['class'] ) ) {
+				if ( $check_class && $ignore_class == $call['class'] )
+					continue; // Filter out calls
+
+				$caller[] = "{$call['class']}{$call['type']}{$call['function']}";
+			} else {
+				if ( in_array( $call['function'], array( 'do_action', 'apply_filters' ) ) ) {
+					$caller[] = "{$call['function']}('{$call['args'][0]}')";
+				} elseif ( in_array( $call['function'], array( 'include', 'include_once', 'require', 'require_once' ) ) ) {
+					$caller[] = $call['function'] . "('" . str_replace( array( WP_CONTENT_DIR, ABSPATH ) , '', $call['args'][0] ) . "')";
+				} else {
+					$caller[] = $call['function'];
+				}
+			}
+		}
+		if ( $pretty )
+			return join( ', ', array_reverse( $caller ) );
+		else
+			return $caller;
+	}
 }
