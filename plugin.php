22205f74e784ac7a9d597366bdd057abb576390f
Nathaniel Schweinberg <natanielks@gmail.com>
2019-01-23 13:38:39 -0800

    check if functions are available. if not, exit early

--- plugin.php
+++ plugin.php
@@ -9,6 +9,12 @@
 
 namespace HM\Platform\XRay;
 
+if ( ! function_exists( __NAMESPACE__ . '\\bootstrap' ) ) {
+	// Exit early in the event functions are not available. This likely means X-Ray is
+	// disabled in your environment.
+	return;
+}
+
 add_filter( 'query', __NAMESPACE__ . '\\filter_mysql_query' );
 add_action( 'requests-requests.before_request', __NAMESPACE__ . '\\trace_requests_request', 10, 5 );
 add_filter( 'hm_platform_cloudwatch_error_handler_error', __NAMESPACE__ . '\\on_cloudwatch_error_handler_error' );
