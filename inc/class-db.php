1b2be43ccb058d4a8e6767c4390aaacb951776e3
Chris Reynolds <chris@jazzsequence.com>
2021-04-26 15:46:19 -0600

    fix comment

--- inc/class-db.php
+++ inc/class-db.php
@@ -33,22 +33,22 @@ class DB extends wpdb {
 	}
 
 	/**
-	* Return a comma-separated string of functions that have been called to get
-	* to the current point in code.
-	*
-	* @since 3.4.0
-	*
-	* @see https://core.trac.wordpress.org/ticket/19589
-	*
-	* @param string $ignore_class Optional. A class to ignore all function calls within - useful
-	*                             when you want to just give info about the callee. Default null.
-	* @param int    $skip_frames  Optional. A number of stack frames to skip - useful for unwinding
-	*                             back to the source of the issue. Default 0.
-	* @param bool   $pretty       Optional. Whether or not you want a comma separated string or raw
-	*                             array returned. Default true.
-	* @return string|array Either a string containing a reversed comma separated trace or an array
-	*                      of individual calls.
-	*/
+	 * Return a comma-separated string of functions that have been called to get
+	 * to the current point in code.
+	 *
+	 * @since 3.4.0
+	 *
+	 * @see https://core.trac.wordpress.org/ticket/19589
+	 *
+	 * @param string $ignore_class Optional. A class to ignore all function calls within - useful
+	 *                             when you want to just give info about the callee. Default null.
+	 * @param int    $skip_frames  Optional. A number of stack frames to skip - useful for unwinding
+	 *                             back to the source of the issue. Default 0.
+	 * @param bool   $pretty       Optional. Whether or not you want a comma separated string or raw
+	 *                             array returned. Default true.
+	 * @return string|array Either a string containing a reversed comma separated trace or an array
+	 *                      of individual calls.
+	 */
 	function wp_debug_backtrace_summary( $ignore_class = null, $skip_frames = 0, $pretty = true ) {
 		if ( version_compare( PHP_VERSION, '5.2.5', '>=' ) ) {
 			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_debug_backtrace
